b5cb46527843b4f443e1ac3afc08d2ed38d59d25
Wrk-Sam <hammerassault@gmail.com>
2017-10-26 10:05:06 -0200

    Formatando atributo idade de pessoasComAmigos

--- variableAndDataTypes.js
+++ variableAndDataTypes.js
@@ -9,7 +9,7 @@ var Famosos = ['Google', 'Twwiter', 'Facebook', 'Instagram'];
 
 var pessoaComAmigos = {
     nome : 'Falsto',
-    idade : 56 + 'Anos',
+    idade : 56 + ' Anos',
     bordao : 'Oloco Meu',
     Peso : 99.9 + 'Kg',
     Amigos : Famosos
