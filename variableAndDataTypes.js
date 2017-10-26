51ab7911a4a47ce67793ee413eeae574eb3f9d45
Wrk-Sam <hammerassault@gmail.com>
2017-10-26 10:00:50 -0200

    Agora misturando Objetos e Arrays

--- variableAndDataTypes.js
+++ variableAndDataTypes.js
@@ -6,3 +6,11 @@ var Pessoa = {
 }
 
 var Famosos = ['Google', 'Twwiter', 'Facebook', 'Instagram'];
+
+var pessoaComAmigos = {
+    nome : 'Falsto',
+    idade : 56 + 'Anos',
+    bordao : 'Oloco Meu',
+    Peso : 99.9 + 'Kg',
+    Amigos : Famosos
+}
